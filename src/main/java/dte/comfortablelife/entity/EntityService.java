d9c80224e90041d66ee0c1621fb5f764cb8090cc
David <darknight4123@gmail.com>
2021-05-28 13:28:21 +0300

    Simplified EntityService

--- src/main/java/dte/comfortablelife/entity/EntityService.java
+++ src/main/java/dte/comfortablelife/entity/EntityService.java
@@ -2,11 +2,23 @@ package dte.comfortablelife.entity;
 
 import java.util.Set;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.event.entity.EntityEvent;
 
 public interface EntityService
 {
-	Set<EntityType> getTreatedTypes();
 	void despawnAll();
 	void preventNextSpawns();
+	boolean shouldTreat(EntityType entity);
+	Set<EntityType> getTreatedTypes();
+	
+	default boolean shouldTreat(EntityEvent event) 
+	{
+		return shouldTreat(event.getEntityType());
+	}
+	default boolean shouldTreat(Entity entity) 
+	{
+		return shouldTreat(entity.getType());
+	}
 }
