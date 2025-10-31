038de88b964282be5ea62d86f0df18443f777efe
David <darknight4123@gmail.com>
2025-10-31 04:01:45 +0200

    Removed AnnoyanceHandlerProvider

--- src/main/java/dte/comfortablelife/ComfortableLife.java
+++ src/main/java/dte/comfortablelife/ComfortableLife.java
@@ -1,8 +1,19 @@
 package dte.comfortablelife;
 
 import dte.comfortablelife.annoyancehandler.AnnoyanceHandler;
-import dte.comfortablelife.annoyancehandler.AnnoyanceHandlerProvider;
+import dte.comfortablelife.annoyancehandler.EntityBlacklistHandler;
+import dte.comfortablelife.annoyancehandler.StormHandler;
 import dte.modernjavaplugin.ModernJavaPlugin;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.EntityType;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toSet;
+import static org.bukkit.ChatColor.RED;
 
 public class ComfortableLife extends ModernJavaPlugin
 {
@@ -10,13 +21,56 @@ public class ComfortableLife extends ModernJavaPlugin
 	public void onEnable()
 	{
 		saveDefaultConfig();
-		stopAnnoyances();
+        handleAnnoyances();
 	}
 
-	private void stopAnnoyances()
-	{
-		AnnoyanceHandlerProvider provider = new AnnoyanceHandlerProvider(getConfig(), this);
+    private void handleAnnoyances()
+    {
+        Stream.of(parseEntityBlacklistHandler(), parseStormHandler())
+                .filter(Optional::isPresent) //empty optional means the handler was disabled in the config
+                .map(Optional::get)
+                .forEach(AnnoyanceHandler::stop);
+    }
 
-		provider.getHandlers().forEach(AnnoyanceHandler::stop);
-	}
+    private Optional<AnnoyanceHandler> parseEntityBlacklistHandler()
+    {
+        return getActiveHandlerSection("entity")
+                .map(section ->
+                {
+                    Set<EntityType> blacklist = section.getStringList("blacklist").stream()
+                            .map(typeName ->
+                            {
+                                try
+                                {
+                                    return EntityType.valueOf(typeName.toUpperCase().replace(' ', '_'));
+                                }
+                                catch(IllegalArgumentException exception)
+                                {
+                                    logToConsole(RED + String.format("Couldn't blacklist \"%s\" because such mob doesn't exist!", typeName));
+                                    return null;
+                                }
+                            })
+                            .filter(Objects::nonNull)
+                            .collect(toSet());
+
+                    return new EntityBlacklistHandler(blacklist, this);
+                });
+    }
+
+    private Optional<AnnoyanceHandler> parseStormHandler()
+    {
+        return getActiveHandlerSection("storm")
+                .map(unused -> new StormHandler(this));
+    }
+
+    private Optional<ConfigurationSection> getActiveHandlerSection(String handlerName)
+    {
+        ConfigurationSection section = getConfig().getConfigurationSection("handlers." + handlerName);
+
+        //return null if the handler is disabled
+        if(!section.getBoolean("active"))
+            return Optional.empty();
+
+        return Optional.of(section);
+    }
 }
