922edf925f8eda0478bf6313ebb8703060294a82
David <darknight4123@gmail.com>
2025-10-31 03:05:38 +0200

    Fixed loaded blacklisted entities not getting despawned

--- src/main/java/dte/comfortablelife/ComfortableLife.java
+++ src/main/java/dte/comfortablelife/ComfortableLife.java
@@ -10,7 +10,6 @@ public class ComfortableLife extends ModernJavaPlugin
 	public void onEnable()
 	{
 		saveDefaultConfig();
-
 		stopAnnoyances();
 	}
 
--- src/main/java/dte/comfortablelife/annoyancehandler/EntitiesHandler.java
+++ src/main/java/dte/comfortablelife/annoyancehandler/EntitiesHandler.java
@@ -1,17 +1,16 @@
 package dte.comfortablelife.annoyancehandler;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 import dte.modernjavaplugin.ModernJavaPlugin;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.world.ChunkLoadEvent;
+
+import static java.util.stream.Collectors.toList;
 
 public class EntitiesHandler implements AnnoyanceHandler
 {
@@ -27,27 +26,23 @@ public class EntitiesHandler implements AnnoyanceHandler
 	@Override
 	public void stop()
 	{
-		Bukkit.getPluginManager().registerEvents(new Listener()
-		{
-			@EventHandler(priority = EventPriority.HIGHEST)
-			public void preventSpawn(CreatureSpawnEvent event)
-			{
-				if(isBlacklisted(event.getEntity()))
-					event.setCancelled(true);
-			}
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this.plugin, () ->
+        {
+            for(World world : Bukkit.getWorlds())
+            {
+                if(world.getPlayers().isEmpty())
+                    return;
 
-			@EventHandler(priority = EventPriority.HIGHEST)
-			public void despawnOffChunk(ChunkLoadEvent event)
-			{
-				Arrays.stream(event.getChunk().getEntities())
-						.filter(entity -> isBlacklisted(entity))
-						.forEach(Entity::remove);
-			}
-		}, this.plugin);
+                getBlacklistedEntitiesAt(world).forEach(Entity::remove);
+            }
+        }, 0, 5);
 	}
 
-	private boolean isBlacklisted(Entity entity) 
-	{
-		return this.blacklist.contains(entity.getType());
-	}
+    private List<Entity> getBlacklistedEntitiesAt(World world)
+    {
+        return Arrays.stream(world.getLoadedChunks())
+                .flatMap(chunk -> Arrays.stream(chunk.getEntities()))
+                .filter(entity -> this.blacklist.contains(entity.getType()))
+                .collect(toList());
+    }
 }
